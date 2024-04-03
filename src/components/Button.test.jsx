21bb14f84237507428ebe2fcc58a505895e8bacd
DEXTERNATAN <pollyanne317@gmail.com>
2024-04-03 11:30:37 -0300

    Adiciona testes de componentes ao projeto

---
+++ src/components/Button.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrButton } from '@govbr-ds-testing/webcomponents-react';
+
+describe('BrButton component', () => {
+  it('renders button with correct type, emphasis, and text', () => {
+    render(
+      <BrButton type="reset" emphasis="secondary" className="mr-3">
+        Cancelar
+      </BrButton>
+    );
+
+    const buttonElement = screen.getByText('Cancelar');
+
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveClass('mr-3');
+    expect(buttonElement.tagName).toBe('BUTTON');
+    expect(buttonElement).toHaveAttribute('type', 'reset');
+    expect(buttonElement).toHaveAttribute('emphasis', 'secondary')
+    
+    
+    // screen.debug()
+  });
+});
